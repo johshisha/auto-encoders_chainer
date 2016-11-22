768ddee5cf6f614c914ea31b554e4cf5de02b973
johshisha <johshisha@gmail.com>
2016-11-23 04:10:31 +0900

    allow args

--- evaluater.py
+++ evaluater.py
@@ -4,24 +4,59 @@ import chainer
 from chainer import computational_graph, serializers, cuda, optimizers, Variable
 from chainer import functions as F, links as L
 import numpy as np
-import random
+import random, argparse
+
 
 from util import util
-from model.auto_encoder import AutoEncoder
-
-saved_model = 'resource/auto_encoder.model'
-
-x_train, x_test, y_train, y_test = util.load_mnist(N=10000) #load mnist data
-
-model = AutoEncoder()
-serializers.load_npz(saved_model, model)
-model.train = False
+from model import auto_encoder, deep_auto_encoder
 
 
-#show test sample estimation
-n_sample = 32
-indexs = random.sample(range(len(y_test)), n_sample)#抽出する添字を取得
-x, t = Variable(x_test[indexs]), Variable(y_test[indexs])
-y = model(x if n_sample != 1 else F.reshape(x, (n_sample, x.data.shape[0])))
-util.draw_digits(y, t)
+archs = {
+    'normal': auto_encoder.AutoEncoder,
+    'deep': deep_auto_encoder.DeepAutoEncoder
+}
+
+
+
+def show_reconstruction(model, x_test, y_test, batchsize):
+    #show test sample estimation
+    indexs = random.sample(range(len(y_test)), batchsize)#抽出する添字を取得
+    x, t = Variable(x_test[indexs]), Variable(y_test[indexs])
+    y = model(x if batchsize != 1 else F.reshape(x, (batchsize, x.data.shape[0])))
+    util.draw_digits(y, t)
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--batchsize', '-B', type=int, default=32,
+                        help='Learning minibatch size')
+    parser.add_argument('--epoch', '-E', type=int, default=10,
+                        help='Number of epochs to train')
+    parser.add_argument('--arch', '-a', choices=archs.keys(), default='normal',
+                        help='Auto-encoder architecture')
+    parser.add_argument('--gpu', '-g', type=int, default=-1,
+                        help='GPU ID (negative value indicates CPU')
+    parser.add_argument('--out', '-o', default='resource',
+                        help='Output directory')
+    args = parser.parse_args()
+
+
+    saved_model = '%s/%s.model'%(args.out, args.arch)
+    batchsize = args.batchsize
+
+    x_train, x_test, y_train, y_test = util.load_mnist(noised=False) #load mnist data
+
+
+    #difine model and optimizer
+    model = archs[args.arch]()
+    if args.gpu >= 0:
+        model.to_gpu()
+    else:
+        model.to_cpu()
+
+    serializers.load_npz(saved_model, model)
+    model.train = False
+
+    show_reconstruction(model, x_test, y_test, batchsize)
+
 
--- trainer.py
+++ trainer.py
@@ -4,49 +4,83 @@ import chainer
 from chainer import computational_graph, serializers, cuda, optimizers, Variable
 from chainer import functions as F, links as L
 import numpy as np
+import argparse
 
 
 from util import util
-from model.auto_encoder import AutoEncoder
+from model import auto_encoder, deep_auto_encoder
 from model.losser import Losser
 
+
+archs = {
+    'normal': auto_encoder.AutoEncoder,
+    'deep': deep_auto_encoder.DeepAutoEncoder
+}
 
 
 n_epoch = 10
 batchsize = 32
 
-save_model = 'resource/auto_encoder.model'
+def train(model, optimizer, x_train, y_train):
+    n_train = len(x_train)
+    losser = Losser(model)
+    epoch_loss = []
+    for epoch in range(n_epoch):
+        sum_loss = np.float32(0)
+        for i in range(0, n_train, batchsize):
+            x_batch = Variable(x_train[i:i+batchsize])
+            y_batch = Variable(y_train[i:i+batchsize])
 
-x_train, x_test, y_train, y_test = util.load_mnist(noised=False) #load mnist data
+            optimizer.zero_grads()
+            loss = losser(x_batch, y_batch)
+            loss.backward()
+            optimizer.update()
+
+            sum_loss += float(cuda.to_cpu(loss.data)) * batchsize
+
+        sum_loss /= (i+batchsize)
+        print('epoch %d done, epoch loss is %f'%(epoch, sum_loss))
+        epoch_loss.append(sum_loss)
+
+    return model
 
 
-#difine model and optimizer
-model = AutoEncoder()
-optimizer = optimizers.Adam()
-optimizer.setup(model)
-losser = Losser(model)
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--batchsize', '-B', type=int, default=32,
+                        help='Learning minibatch size')
+    parser.add_argument('--epoch', '-E', type=int, default=10,
+                        help='Number of epochs to train')
+    parser.add_argument('--arch', '-a', choices=archs.keys(), default='normal',
+                        help='Auto-encoder architecture')
+    parser.add_argument('--gpu', '-g', type=int, default=-1,
+                        help='GPU ID (negative value indicates CPU')
+    parser.add_argument('--out', '-o', default='resource',
+                        help='Output directory')
+    args = parser.parse_args()
 
 
-n_train = len(x_train)
+    print('learning %s auto-encoder'%args.arch)
 
-epoch_loss = []
-for epoch in range(n_epoch):
-    sum_loss = np.float32(0)
-    for i in range(0, n_train, batchsize):
-        x_batch = Variable(x_train[i:i+batchsize])
-        y_batch = Variable(y_train[i:i+batchsize])
+    save_model = '%s/%s.model'%(args.out, args.arch)
+    n_epoch = args.epoch
+    batchsize = args.batchsize
 
-        optimizer.zero_grads()
-        loss = losser(x_batch, y_batch)
-        loss.backward()
-        optimizer.update()
+    x_train, x_test, y_train, y_test = util.load_mnist(noised=False) #load mnist data
 
-        sum_loss += float(cuda.to_cpu(loss.data)) * batchsize
 
-    sum_loss /= (i+batchsize)
-    print('epoch %d done, epoch loss is %f'%(epoch, sum_loss))
-    epoch_loss.append(sum_loss)
+    #difine model and optimizer
+    model = archs[args.arch]()
+    if args.gpu >= 0:
+        model.to_gpu()
+    else:
+        model.to_cpu()
 
-serializers.save_npz(save_model, model)
+    optimizer = optimizers.Adam()
+    optimizer.setup(model)
+
+    model = train(model, optimizer, x_train, y_train)
+
+    serializers.save_npz(save_model, model)
 
 
