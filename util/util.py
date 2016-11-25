659ed31cccb37237aca8a995e6f45be1d012275a
johshisha <johshisha@gmail.com>
2016-11-25 19:02:02 +0900

    shuffle dataset

--- util/util.py
+++ util/util.py
@@ -4,6 +4,7 @@ from sklearn.datasets import fetch_mldata
 import numpy as np
 import matplotlib.pyplot as plt
 from chainer import computational_graph as c
+import random
 
 def load_mnist(N = 60000, noised=True):
     # MNISTの手書き数字データのダウンロード
@@ -14,9 +15,13 @@ def load_mnist(N = 60000, noised=True):
     # mnist.data : 70,000件の784次元ベクトルデータ
     mnist.data   = mnist.data.astype(np.float32)
     mnist.data  /= 255     # 0-1のデータに変換
+    random.seed(0)
+    random.shuffle(mnist.data)
 
     # mnist.target : 正解データ（教師データ）
     mnist.target = mnist.target.astype(np.int32)
+    random.seed(0)
+    random.shuffle(mnist.target)
 
 
     # 学習用データを N個、検証用データを残りの個数と設定
